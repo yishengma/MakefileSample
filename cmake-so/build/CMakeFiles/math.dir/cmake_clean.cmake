8445b467f7cabe8ed23ede4ab820ac2fb62663bc
mayisheng <13104862372@163.com>
2020-06-06 21:05:09 +0800

    Cmake 生成so

---
+++ cmake-so/build/CMakeFiles/math.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../lib/libmath.pdb"
+  "../lib/libmath.so"
+  "CMakeFiles/math.dir/src/hello.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/math.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
