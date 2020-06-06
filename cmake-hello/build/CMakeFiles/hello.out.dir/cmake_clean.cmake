77cd4313f3326e6aa6c7199d4e8b3250ef2087d6
mayisheng <13104862372@163.com>
2020-06-06 20:12:43 +0800

    Cmake编写

---
+++ cmake-hello/build/CMakeFiles/hello.out.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello.out.dir/hello.cpp.o"
+  "hello.out"
+  "hello.out.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-hello/hello.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+
+
+int main(int argc,char* argcv[]) {
+    int a = 20;
+    int b = 10;
+    printf("%d+%d",a,b);
+    return 0;
+}
